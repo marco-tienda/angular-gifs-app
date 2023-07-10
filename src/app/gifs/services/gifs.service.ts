bd0c9527ecccbb4ebedffea376c0330d88becaee
Desarrollador Gemp <desarrollador.gemp@gmail.com>
2023-07-10 14:32:59 -0600

    feat: get gifs on service

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,72 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
+  public gifList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
+  private apiKey: string = "1nJ6so6FQ5aB7q02Idxe9L2bIJB5n6u2";
+  private serviceUrl: string = "https://api.giphy.com/v1/gifs";
+
+  constructor(private http: HttpClient) {
+    this.loadLocaleStorage();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string): void {
-    this._tagsHistory.unshift(tag);
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
 
-    console.log(this._tagsHistory);
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this.tagsHistory.splice(0, 10);
+    this.saveLocaleStorage();
+  }
+
+  private saveLocaleStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocaleStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!); // Siempre va a venir una data
+
+    /**
+     * Verificamos que el arreglo que almacena nuestros gifs
+     * contenga algún elemento
+     */
+    if (this._tagsHistory.length === 0) return;
+
+    /**
+     * Invocamos nuestro método para hacer la búsqueda en caso
+     * de que exista un elemento en nuestro arreglo de gifs
+     */
+    this.searchTag(this._tagsHistory[0]);
+  }
+
+  searchTag(tag: string): void {
+    if (tag.length === 0) return;
+
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe(resp => {
+        this.gifList = resp.data;
+      })
+
   }
 
 }
